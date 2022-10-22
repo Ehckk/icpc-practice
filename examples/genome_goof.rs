00832074e0c7eb1b0cf4b7708456494f2a2f2843
Mason Ginter <mason@dagint.com>
2022-10-21 21:29:47 -0400

    Take out helpers in example

--- examples/genome_goof.rs
+++ examples/genome_goof.rs
@@ -2,28 +2,17 @@ use std::time::Instant;
 
 use icpc_stuff::goof::{recursive_goof, goof};
 
-fn test_string_goof<S: AsRef<str>>(s: S) -> usize {
-    let chars: Vec<char> = s.as_ref().chars().collect();
-    let len = goof(&chars);
-    len
-}
-
-fn test_string_goof_recursive<S: AsRef<str>>(s: S) -> usize {
-    let chars: Vec<char> = s.as_ref().chars().collect();
-    let len = recursive_goof(&chars);
-    len
-}
-
 fn test_str<S: AsRef<str>>(s: S) {
     let s = s.as_ref();
     println!("Testing {:?}", s);
+    let chars: Vec<char> = s.chars().collect();
     // Do it non-recursively
     let start = Instant::now();
-    let count = test_string_goof(s);
+    let count = goof(&chars);
     println!("{:?} in order to get {count}", start.elapsed());
     // Do it recursively
     let start = Instant::now();
-    let count = test_string_goof_recursive(s);
+    let count = recursive_goof(&chars);
     println!("{:?} in order to get {count} recursively", start.elapsed());
 }
 
