c46532b6a4a31b19153bd6ccbef58994f789401d
JacobPStephens <jacobpstephens9@gmail.com>
2023-04-14 19:11:27 -0400

    Bounding Robots Solution

--- Jacob/free.py
+++ Jacob/free.py
@@ -1,13 +1,11 @@
-'''
-combo = ['A', 'R', 'B', 'L']
-if len(combo) == 4:
-    combo.pop(0)
-    if (combo.count('R') == 1) and (combo.count('B') == 1) and (combo.count('L') == 1):
-        print('COMBO FOUND')
-'''
+graph = {
+    'a' : 1,
+    'b' : 2,
+    'c' : 3
+}
 
-output = "RRRBL"
-output = output[:-3]
-output += 'C'
 
-print(output)
+if 'd' in graph:
+    print('in graph')
+else:
+    print('not in graph')
