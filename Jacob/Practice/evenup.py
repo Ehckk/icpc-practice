18dae3c2c523a745584050ad8e8cec9b30d08c72
JacobPStephens <124077831+JacobPStephens@users.noreply.github.com>
2023-09-18 23:24:53 -0400

    Even Up Solitaire

--- Jacob/Practice/evenup.py
+++ Jacob/Practice/evenup.py
@@ -1,3 +1,33 @@
+''' 
+Jacob Stephens, ICPC
+September 18, 2023
+Even Up Solitaire https://open.kattis.com/problems/evenup
+Steven Halim Book pg. 77 (stacks, #1)
+
+AC
+'''
+
+input()
+
+main = [int(i) for i in input().split()]
+aux = [-500]
+moves = 0
+
+while main:
+    if main[-1] == aux[-1]:
+        main.pop()
+        aux.pop()
+        moves += 1
+    else:
+        aux.append(main.pop())
+        moves += 1
+
+if len(aux) == 1:
+    print(moves)
+else:
+    print('impossible')
+
+    
 input()
 cards = input().split()
 cards = [int(i) for i in cards]
