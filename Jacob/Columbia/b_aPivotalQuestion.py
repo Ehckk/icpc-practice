28f34aca9e8a711bafd38b98f559c67c5c7b4b44
JacobPStephens <124077831+JacobPStephens@users.noreply.github.com>
2023-11-01 23:31:31 -0400

    A Pivotal Question
    finalized solution of problem B

--- Jacob/Columbia/b_aPivotalQuestion.py
+++ Jacob/Columbia/b_aPivotalQuestion.py
@@ -1,12 +1,25 @@
 # Jacob Stephens, October 31, 2023
+# Problem B: A Pivotal Question (GNYR ICPC 2023)
 
-line = [int(i) for i in input().split()[1:]]
-p = []
-sortedLine = sorted(line)
-for i in range(len(line)):
-    if line[i] == sortedLine[i]:
-        p.append(line[i])
+A = [int(i) for i in input().split()[1:]]
 
-# output
-print(len(p), end= ' ')
-print("".join(int(i) for i in p))
+maxval = -float('inf')
+minval = float('inf')
+mins = []
+maxes = []
+
+# traverse forward and backward through list,
+# updating running max and min values
+for i in range(len(A)):
+    if A[i] > maxval:
+        maxes.append(A[i])
+        maxval = A[i]
+
+    x = A[len(A)-1-i]
+    if x < minval:
+        mins.append(x)
+        minval = x
+
+# calc pivots, and output
+pivots = [str(n) for n in maxes if n in mins]
+print(" ".join(pivots))
