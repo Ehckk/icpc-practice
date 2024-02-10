bfcd77355c59973bd5fb553f5b05950eb40098b7
Amilcar Armmand <armmand@gmail.com>
2024-02-09 21:53:43 -0500

    prints correct values

---
+++ amilcar/heirsdilema.py
@@ -0,0 +1,31 @@
+# Heir's Dilema
+# https://open.kattis.com/problems/heirsdilemma
+
+def main():
+    combos = 0
+
+    # input session number range.
+    low, high = input('Enter range: ').strip().split()
+
+    for num in range(int(low), int(high) + 1):
+        num_list = [int(ch) for ch in str(num)]
+
+        # test for repeated digits and zeros in number
+        if len(num_list) != len(set(num_list)) or 0 in num_list:
+            continue
+
+        # test if each digit is a factor of num
+        is_factor = True
+        for i in num_list:
+            if num % i != 0:
+                is_factor = False
+                break
+        if is_factor:
+            combos += 1
+
+    print(combos)
+
+
+if __name__ == '__main__':
+    main()
+
